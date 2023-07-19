1d336729d1a88984a59e2c247cb95f3f4a379e2a
DEliza <32403146+Elizaddh@users.noreply.github.com>
2023-07-19 10:56:23 -0400

    Add files via upload

---
+++ R/bods.R
@@ -0,0 +1,17 @@
+#' Dataset from gage package
+#'
+#' Chart for latest genome and annotation package for few selected species that
+#' are downloaded when no reference is provided by user
+#'
+#' @format ## `bods` A dataframe with x rows and y columns;
+#' rows describe different species
+#' \describe{
+#'   \item{genome}{Name of the genome package}
+#'   \item{species}{Scientific names of species}
+#'   \item{annotation}{Name of the annotation species}
+#'   ...
+#' }
+#' @source gage
+"bods"
+
+
