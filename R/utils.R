f52a7bc94f35cbfe06188ab88a6b886d37414283
DEliza <32403146+Elizaddh@users.noreply.github.com>
2023-09-08 09:56:23 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -14,128 +14,41 @@
 #' @importFrom methods is
 #'
 
-sanity_check <- function(ref.dir, outdir, pos = 1, entity, corenum, compare,
-                        rerun) {
-    if (file.exists(outdir) & rerun == FALSE) {
-        unlink(outdir, recursive = TRUE)
-    }
-
-    if (!file.exists(outdir)) {
-        # default output file
-        dir.create(outdir)
-    }
-    result.dir <- outdir
-    message(paste0("The results will be organized in ", result.dir, 
-            collapse = "|"))
-    # setwd(outdir)
-
-    # check and create dir for organizing results
-    checkcretdir <- function(parentname, dirname) {
-        if (!file.exists(file.path(parentname, dirname))) {
-            dir.create(file.path(parentname, dirname))
-        }
-        assign(dirname,
-            value = file.path(parentname, dirname),
-            envir = as.environment(pos)
-        ) # .GlobalEnv)#environment())
-    }
-
-    folder_to_create <- list(
-        "fastqc_results", "fastp_results", "gage_results",
-        "differential_analysis", "aligned_bam"
-    )
-    trim_dir <- list("fastp_log", "unpaired")
-    diff_dir <- list("DESeq2", "edgeR")
-    pathway_types <- list("KEGG", "GO")
-    kegg_types <- list("signalling", "metabolism", "disease", "sig_n_met")
-    go_types <- list(
-        "biological_process", "molecular_function",
-        "cellular_component"
-    )
-    lapply(folder_to_create, checkcretdir, parentname = result.dir)
-    lapply(trim_dir, checkcretdir, parentname = file.path(
-        result.dir,
-        "fastp_results"
-    ))
-    lapply(diff_dir, checkcretdir, parentname = file.path(
-        result.dir,
-        "differential_analysis"
-    ))
-    lapply(pathway_types, checkcretdir, parentname = file.path(
-        result.dir,
-        "gage_results"
-    ))
-    lapply(kegg_types, checkcretdir, parentname = file.path(
-        result.dir,
-        "gage_results", "KEGG"
-    ))
-    lapply(go_types, checkcretdir, parentname = file.path(
-        result.dir,
-        "gage_results", "GO"
-    ))
-
-    # just to make sure rest of codes are same
-    qc.dir <- fastqc_results
-    diff.dir <- differential_analysis
-    trim.dir <- fastp_results
-    gage.dir <- gage_results
-    trim.log <- fastp_log
-    edger.dir <- edgeR
-    message(edger.dir)
-    deseq2.dir <- DESeq2
-    kegg.dir <- KEGG
-    go.dir <- GO
-
+sanity_check <- function(ref.dir, outdir, pos = 1, entity, corenum, compare){
     # References
     # if only species name is given and both geneAnnotation and genome is NULL
     if (is.na(ref.dir)) {
         data(anntpkglist, package = "pathviewwrap", envir = environment())
         ref_info <- anntpkglist
-
         species_no <- which(ref_info$species == entity)
         annotate_pkg <- ref_info$annotation[species_no]
         genome_pkg <- ref_info$genome[species_no]
-
         # (set of genome and annotation pkg come from developers list)
-        #
         sqlite.md5 <- paste0(annotate_pkg, ".sqlite.md5")
-        sqlite.SpliceSites.txt.md5 <- paste0(
-            annotate_pkg,
-            ".sqlite.SpliceSites.txt.md5"
-        )
-        sqlite.SpliceSites.txt <- paste0(
-            annotate_pkg,
-            ".sqlite.SpliceSites.txt"
-        )
-        if (file.exists(file.path(
-            .libPaths()[1],
+        sqlite.SpliceSites.txt.md5 <- paste0(annotate_pkg,
+                                             ".sqlite.SpliceSites.txt.md5")
+        sqlite.SpliceSites.txt <- paste0(annotate_pkg,
+            ".sqlite.SpliceSites.txt")
+        if (file.exists(file.path(.libPaths()[1],
             annotate_pkg, "extdata", sqlite.md5
         ))) {
-            unlink(file.path(
-                .libPaths()[1],
+            unlink(file.path(.libPaths()[1],
                 annotate_pkg, "extdata", sqlite.md5
-            ))
-        }
-        if (file.exists(file.path(
-            .libPaths()[1],
+            ))}
+        if (file.exists(file.path(.libPaths()[1],
             annotate_pkg, "extdata",
             sqlite.SpliceSites.txt.md5
         ))) {
-            unlink(file.path(
-                .libPaths()[1], annotate_pkg,
+            unlink(file.path( .libPaths()[1], annotate_pkg,
                 "extdata", sqlite.SpliceSites.txt.md5
-            ))
-        }
-        if (file.exists(file.path(
-            .libPaths()[1], annotate_pkg,
+            ))}
+        if (file.exists(file.path(.libPaths()[1], annotate_pkg,
             "extdata", sqlite.SpliceSites.txt
         ))) {
             unlink(file.path(
                 .libPaths()[1], annotate_pkg,
                 "extdata", sqlite.SpliceSites.txt
-            ))
-        }
-
+            ))}
         # annotation pkg installation
         pkg.on <- requireNamespace(annotate_pkg,
             lib.loc = .libPaths()[1],
@@ -146,18 +59,13 @@ sanity_check <- function(ref.dir, outdir, pos = 1, entity, corenum, compare,
         Intall the required package with the following command,
         > BiocManager::install('", annotate_pkg, "'
                         ,force = TRUE,
-                        lib.loc = .libPaths()[1]  )", collapse = "|"))
+                        lib.loc = .libPaths()[1]  )"))
             return(invisible(NULL)) # needs reevaluation
-            # pkg.on = requireNamespace(annotate_pkg,  lib.loc = .libPaths()[1])
-            # if (!pkg.on)
-            #  stop(paste("Fail to install/load gene annotation package
-            # ",annotate_pkg, "!", sep = ""))
         }
         geneAnnotation <- file.path(
             .libPaths()[1], annotate_pkg,
             "extdata", paste0(annotate_pkg, ".sqlite")
         )
-
         # genome file installation
         genomeFile <- genome_pkg
         pkg.on <- requireNamespace(genome_pkg,
@@ -170,7 +78,7 @@ sanity_check <- function(ref.dir, outdir, pos = 1, entity, corenum, compare,
         Intall the required package with the following command,
         >  BiocManager::install('",
                 genome_pkg, "',force = TRUE,
-                        lib.loc = .libPaths()[1] )", collapse = "|"
+                        lib.loc = .libPaths()[1] )"
             ))
             return(invisible(NULL))
         }
@@ -187,20 +95,7 @@ sanity_check <- function(ref.dir, outdir, pos = 1, entity, corenum, compare,
         geneAnnotation <- list.files(ref.dir, ".gtf$|.gff$", full.names = TRUE)
         ## could be changed to include gtf, gff etc,check with quasR package
         message(geneAnnotation)
+        
+        return(c(genomeFile, geneAnnotation))
     }
-    message(paste0(
-        "this is directory lists,
-        qc.dir :", qc.dir, "\n",
-        "trim.dir :", trim.dir, "\n",
-        "genomeFile :", genomeFile, "\n",
-        "geneAnnotation :", geneAnnotation, "\n",
-        "deseq2.dir :", deseq2.dir, "\n",
-        "edger.dir :", edger.dir, "\n",
-        "gage.dir : ", gage.dir, "\n", collapse = "|"
-    ))
-
-    return(c(
-        qc.dir, trim.dir, genomeFile, geneAnnotation, deseq2.dir,
-        edger.dir, gage.dir
-    ))
 }
