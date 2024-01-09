ab8ab44e21573844f27ec86aa2b86e173f40daf8
DEliza <32403146+Elizaddh@users.noreply.github.com>
2024-01-09 15:57:36 -0500

    Update path_rna_GAGE.R

--- R/path_rna_GAGE.R
+++ R/path_rna_GAGE.R
@@ -20,7 +20,8 @@
 #' @import pathview
 #' @return nothing returned
 #'
-run_gage <- function(gsets, work.dir, same.dir,compare = compare) {
+run_gage <- function(gsets, work.dir, same.dir,compare = compare,
+                     fc_matrix = exp.fc, entity)) {
     fc.kegg.p <- gage( exp.fc, gsets = gsets, ref = NULL,samp = NULL,
                        same.dir = same.dir, compare = compare)
     sel <- fc.kegg.p$greater[, "q.val"] < 0.1 & !is.na(
