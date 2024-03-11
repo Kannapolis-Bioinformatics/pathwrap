7052c4b65a1cf04ccf356d642a3323f8de3b1f17
DEliza <32403146+Elizaddh@users.noreply.github.com>
2024-03-11 13:12:09 -0400

    Update run_cpathway_analysis.R

--- R/run_cpathway_analysis.R
+++ R/run_cpathway_analysis.R
@@ -1,31 +1,15 @@
-#' Function for running the compound pathway analysis
-#'
-#' This function prepares for running the gage analysis on compound sets
-#'
-#' @param cdatapath :pathway for compound data
-#' @param cpd_id_type type of compound id as compatible to ones in 
-#' pathview::data(rn.list)
-#' @param csamp column number of sample
-#' @param cref column number of reference
-#' @param ccompare comparision type for sample
-#' @param cset_dir directory where results of compound set analysis is stored
-#' @import gage
-#' @import pathview
-#' @return gage_return_obj_c return object from run_gage for compound
-
-
 run_cpathway<- function( cdatapath,cpd_id_type= "KEGG COMPOUND accession",csamp,
                         cref, ccompare="paired" , cset_dir){
     if (grepl(".csv$", x = cdatapath)){
-        cdata <- read.csv(cdatapath, header = T)
+        cdata <- read.csv(cdatapath, header = T, row.names = 1)
     } else{
-        cdata<- read.table(file = cdatapath ,  sep="\t", header= TRUE)
-    }    
+        cdata<- read.table(file = cdatapath,sep="\t",header= TRUE,row.names = 1)
+    }
+    
     if (cpd_id_type!="KEGG COMPOUND accession"){
         cpd_idmap<-cpd2kegg(in.ids=rownames(cdata),in.type=toupper(cpd_id_type))
         didx<-duplicated(cpd_idmap[,1])
         cpd_idmap<-cpd_idmap[!didx,]
-
         cdata <-mol.sum(cdata, cpd_idmap)
     }
     
