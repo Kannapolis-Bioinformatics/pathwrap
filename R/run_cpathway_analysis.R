9bfa1317bdef25aa684ad5b61318caca99703bd2
DEliza <32403146+Elizaddh@users.noreply.github.com>
2024-04-01 21:57:02 -0400

    Update run_cpathway_analysis.R

--- R/run_cpathway_analysis.R
+++ R/run_cpathway_analysis.R
@@ -41,6 +41,7 @@ run_cpathway<- function( cdatapath,cpd_id_type= "KEGG COMPOUND accession",csamp,
         else if (ncref==1) cpd_data<- cpd_data[,csamp]- cpd_data[,cref]
         else cpd_data <-cpd_data[,csamp]- rowMeans(cpd_data[,cref]) }
     csets <- loadcsets()
+    names(csets)<-str_replace_all(names(csets),"^map",kegg.species.code(entity))
     gage_return_obj_c <- run_gage(csets, cset_dir , same.dir = FALSE,
             compare = ccompare, entity= entity, cpd_data, 
             cpd.idtype = "kegg",gene_data=NULL, gene_id_type  =NA)
